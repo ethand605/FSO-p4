02912d5060929e7936f02e106fc5a29450de2543
Ethan Deng <ethand.2091@gmail.com>
2022-06-14 10:30:55 -0700

    added jest testing

---
+++ utils/list_helper.js
@@ -0,0 +1,8 @@
+const totalLikes  = (blogs) => {
+    const reducer = (sum, blog) => sum + blog.likes;
+    return blogs.length == 0 ? 0 : blogs.reduce(reducer, 0);
+}
+
+module.exports = {
+    totalLikes 
+};
